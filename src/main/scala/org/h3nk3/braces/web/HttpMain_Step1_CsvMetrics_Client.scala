58d06bb4f63b01955e52450f93f02d0c889806fb
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-04-03 03:33:14 -0400

    csv example works

--- src/main/scala/org/h3nk3/braces/web/HttpMain_Step1_CsvMetrics_Client.scala
+++ src/main/scala/org/h3nk3/braces/web/HttpMain_Step1_CsvMetrics_Client.scala
@@ -6,9 +6,10 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest}
 import akka.http.scaladsl.server.Directives
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ThrottleMode}
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
 import org.h3nk3.braces.domain._
 
 import scala.concurrent.duration._
@@ -18,7 +19,7 @@ object HttpMain_Step1_CsvMetrics_Client extends App
   with Directives with OurOwnWebSocketSupport
   with CsvDomain {
 
-  implicit val system = ActorSystem("BracesBackend")
+  implicit val system = ActorSystem("BracesBackend", ConfigFactory.parseString(""))
   implicit val materializer = ActorMaterializer()
   implicit val dispatcher = system.dispatcher
 
@@ -32,15 +33,23 @@ object HttpMain_Step1_CsvMetrics_Client extends App
       batteryPower = 30
     )
 
+  
+  /*
+              netstat -n -p tcp | grep 127 | grep 8080
+   */
 
-  // client example:
+  val dataSource = Source.repeat(NotUsed)
+    .mapAsync(1)(_ => Marshal(example).to[ByteString])
+    .intersperse(ByteString("\n"))
+    .throttle(1000, 1.second, 100, ThrottleMode.Shaping)
+    .map { it =>
+      println("> " + System.currentTimeMillis())
+      it
+    }
+  
   Http().singleRequest(
     HttpRequest(HttpMethods.POST, uri = "http://127.0.0.1:8080/drone/csv-example")
-      .withEntity(HttpEntity(
-        ContentTypes.`text/csv(UTF-8)`,
-        Source.repeat(NotUsed)
-          .mapAsync(1)(_ => Marshal(example).to[ByteString])
-      ))
+      .withEntity(HttpEntity(ContentTypes.`text/csv(UTF-8)`, dataSource))
   )
 
 }
